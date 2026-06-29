c0268ca55fa842c77276f31c88b9f01c15a391ba
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-29 13:29:54 -0300

    fix(api): trim user field values

--- apps/api/source/features/users/validations.ts
+++ apps/api/source/features/users/validations.ts
@@ -20,12 +20,14 @@ export const signInSchema = z
 export const userSchema = z.object({
 	username: z
 		.string()
+		.trim()
 		.min(1, 'Username is required')
 		.max(username.length as number, `Username cannot be longer than ${username.length} characters`)
 		.regex(alphanumericRegex, 'Username must only contain alphanumeric characters')
 		.refine(isUsernameAvailable, 'Username is already taken'),
 	password: z
 		.string()
+		.trim()
 		.min(4, 'Password must be at least 4 characters long')
 		.max(100, 'Password cannot be longer than 100 characters'),
 	bestTimeInMs,

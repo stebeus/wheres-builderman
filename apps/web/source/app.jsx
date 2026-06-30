52e2275af46920cec6afa2658c510e91e4b78bd7
stebeus <189894010+stebeus@users.noreply.github.com>
2026-06-30 12:15:49 -0300

    feat(web): set up `App`

---
+++ apps/web/source/app.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from 'react';
+
+import { Footer, Navbar } from './components/index.js';
+import { Authentication, Leaderboard, Success, Welcome } from './components/modals/index.js';
+import { createModal } from './components/ui/index.js';
+
+export const App = () => {
+	const welcomeRef = useRef(null);
+	const authenticationRef = useRef(null);
+	const successRef = useRef(null);
+
+	const [time, setTime] = useState(0);
+
+	const handleAction = () => {
+		authenticationRef.current.close();
+		successRef.current.showModal();
+	};
+
+	const modals = [
+		{
+			id: 'welcome',
+			ref: welcomeRef,
+			children: <Welcome />,
+		},
+		{
+			id: 'leaderboard',
+			children: <Leaderboard />,
+		},
+		{
+			id: 'authentication',
+			closedBy: 'none',
+			ref: authenticationRef,
+			children: <Authentication time={time} onAction={handleAction} />,
+		},
+		{
+			id: 'success',
+			ref: successRef,
+			children: <Success />,
+		},
+	];
+
+	useEffect(() => welcomeRef.current.showModal());
+
+	return (
+		<>
+			<Navbar />
+			<main></main>
+			<Footer />
+			{modals.map(createModal)}
+		</>
+	);
+};

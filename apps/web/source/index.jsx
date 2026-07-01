26b0b072356f26d188ad3ea750884ab662424664
stebeus <189894010+stebeus@users.noreply.github.com>
2026-07-01 15:28:25 -0300

    fix(web)!: use `StrictMode` in specific parts
    Prevent re-renders from closing the `Welcome` modal after the page
    loads, because this bug comes from the data fetching of the characters.

--- apps/web/source/index.jsx
+++ apps/web/source/index.jsx
@@ -1,12 +1,7 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './app.jsx';
 
 import './index.css';
 
-createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+createRoot(document.getElementById('root')).render(<App />);

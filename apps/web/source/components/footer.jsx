96264981b265f900e40caa59bfd1ccefa9b9a21b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-07-03 16:37:02 -0300

    feat(web): expand footer details

--- apps/web/source/components/footer.jsx
+++ apps/web/source/components/footer.jsx
@@ -2,10 +2,25 @@ import { ExternalLink } from './ui/external-link.jsx';
 
 export const Footer = () => (
 	<footer>
+		<ul>
+			<li>
+				<ExternalLink to="https://github.com/stebeus/wheres-builderman">Source code</ExternalLink>
+			</li>
+			<li>
+				<ExternalLink to="https://github.com/stebeus">GitHub profile</ExternalLink>
+			</li>
+		</ul>
+		<p>
+			The game and error pictures were created by, respectively,{' '}
+			<ExternalLink to="https://www.reddit.com/user/yellowt3a/">yellowt3a</ExternalLink> and{' '}
+			<ExternalLink to="https://www.reddit.com/user/Stormcloak_Guard_/">
+				Stormcloak_Guard_
+			</ExternalLink>
+			.
+		</p>
 		<p>
 			© Stebeus 2026.{' '}
 			<ExternalLink to="https://opensource.org/license/mit">MIT License</ExternalLink>
 		</p>
-		<ExternalLink to="https://github.com/stebeus">GitHub</ExternalLink>
 	</footer>
 );

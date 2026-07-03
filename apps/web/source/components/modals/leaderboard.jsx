941b5c427d5fc4fe814a19b8cb632dbc9e14371e
stebeus <189894010+stebeus@users.noreply.github.com>
2026-07-03 16:36:49 -0300

    feat(web): add refresh button to `Leaderboard`

--- apps/web/source/components/modals/leaderboard.jsx
+++ apps/web/source/components/modals/leaderboard.jsx
@@ -1,15 +1,23 @@
-import { CloseButton, ErrorFallback, Loader } from '#root/components/ui/index.js';
+import { useEffect, useState } from 'react';
+
+import { Button, CloseButton, ErrorFallback, Loader } from '#root/components/ui/index.js';
 import { useFetch } from '#root/hooks/fetch.js';
+import { fetchData } from '#root/services/fetch.js';
 import { formatSeconds } from '#root/utilities/formatters.js';
 
-const createScore = ({ id, username, bestTimeInCs }) => (
+const createUser = ({ id, username, bestTimeInCs }) => (
 	<li key={id}>
 		{username} {formatSeconds(bestTimeInCs)}
 	</li>
 );
 
 export const Leaderboard = () => {
+	const [users, setUsers] = useState([]);
 	const { data, isLoading, error } = useFetch('users');
+
+	const handleClick = async () => setUsers(await fetchData('users'));
+
+	useEffect(() => setUsers(data), [data]);
 
 	if (isLoading) return <Loader />;
 	if (error != null) return <ErrorFallback error={error} />;
@@ -17,7 +25,8 @@ export const Leaderboard = () => {
 	return (
 		<>
 			<h2>Leaderboard</h2>
-			<ol>{data.map(createScore)}</ol>
+			<ol>{users?.map(createUser)}</ol>
+			<Button onClick={handleClick}>Reload users</Button>
 			<CloseButton commandFor="leaderboard">Close</CloseButton>
 		</>
 	);

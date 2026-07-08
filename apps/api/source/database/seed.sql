5fedb63c2de8ab482e6b42691b026824dde3b467
stebeus <189894010+stebeus@users.noreply.github.com>
2026-07-08 11:12:16 -0300

    feat(api): add SQL for seeding characters

---
+++ apps/api/source/database/seed.sql
@@ -0,0 +1,91 @@
+-- Paste this to any SQL console
+INSERT INTO
+  characters (name, description, position)
+VALUES
+  (
+    'mrgamewatch_fan',
+    'An enthusiast of the best Nintendo character',
+    point(6, 8)
+  ),
+  (
+    'BIGGER_HEAD',
+    'A red noob with an iconic hat',
+    point(7, 13)
+  ),
+  (
+    'salih1',
+    'The creator of the PolandBall roleplaying game',
+    point(7, 17)
+  ),
+  (
+    'waffler',
+    'A king holding a famous Belgian-French food',
+    point(8, 2)
+  ),
+  (
+    'Cop',
+    'The most hated person in prison games',
+    point(8, 4)
+  ),
+  (
+    'stanoob',
+    'A novice from the Stanford university',
+    point(8, 6)
+  ),
+  (
+    'admint',
+    'That one player who guards servers',
+    point(8, 9)
+  ),
+  (
+    'Mr_Emeraldz',
+    'A rich and green noob',
+    point(8, 10)
+  ),
+  (
+    'yellowt3a',
+    'The creator of the image used in this game',
+    point(8, 12)
+  ),
+  (
+    'Builderman',
+    'A player with a constructor helmet',
+    point(8, 15)
+  ),
+  (
+    'bluejay55669',
+    'The 55669th bird of the Mordecai''s species',
+    point(8, 16)
+  ),
+  ('pugz_pugz', 'A pug mom', point(8, 18)),
+  (
+    'texan_rancher',
+    'A southern herdsman',
+    point(8, 20)
+  ),
+  ('dripnoob', 'A dapper novice', point(8, 22)),
+  (
+    'Batman',
+    'One of the most famous superheroes',
+    point(8, 24)
+  ),
+  (
+    'u238',
+    'Abbreviation of the most common natural isotope of uranium',
+    point(9, 4)
+  ),
+  (
+    'LugiaMaster',
+    'Owner of a legendary Pokémon',
+    point(9, 8)
+  ),
+  (
+    'ThatOneNukeKiller',
+    'The funniest guy in parties',
+    point(9, 11)
+  ),
+  (
+    'lua_junior',
+    'A graduate of the scripting language used in Roblox',
+    point(9, 16)
+  );

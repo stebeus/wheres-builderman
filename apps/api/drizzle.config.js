c3f7a948679c19723ddb2adf7a9084aa5c2c1706
stebeus <189894010+stebeus@users.noreply.github.com>
2026-07-29 09:01:03 -0300

    refactor(api): shorten source folder name

--- apps/api/drizzle.config.js
+++ apps/api/drizzle.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
 
-import { config } from './source/config.js';
+import { config } from './src/config.js';
 
 export default defineConfig({
 	casing: 'snake_case',
 	dialect: 'postgresql',
-	schema: './source/database/schema.js',
+	schema: './src/database/schema.js',
 	dbCredentials: {
 		url: config.DATABASE_URL,
 	},

97f50378630de7d50192feba7299d62af8f28c51
stebeus <189894010+stebeus@users.noreply.github.com>
2026-07-30 10:52:52 -0300

    refactor(api): move db client to dedicated file

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,7 +1,1 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-
-import { env } from '#root/env.ts';
-
-export const db = drizzle({
-	connection: env.DATABASE_URL,
-});
+export * from './client.ts';
